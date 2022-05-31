4d437ca6d2b168ef736ae1fad40e89c461f9e6d2
nas4547 <nas4547@rit.edu>
2022-05-30 22:51:43 -0400

    Added a blacklist feature and scheduled execution.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import datetime
+
 import tweepy
 import config
 import random
+import schedule
 import time
 client = tweepy.Client(config.BEARER_TOKEN, config.API_KEY, config.API_KEY_SECRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET, wait_on_rate_limit='true')
 following = []
 users = []
+blacklist = ['nate64213419', 'StimpyPvP', 'ezPimpy']
 tweet = 'Goodnight @akaTimmay'
 
 
 def find_following():
     global following
+    global blacklist
     temp = client.get_users_following(1197975170, max_results=1000)
     for person in temp.data:
         following.append(person.username)
+    for user in blacklist:
+        following.remove(user)
 
 def pick_users():
     charCount = 0
@@ -34,8 +41,7 @@ def create_tweet():
         tweet += user
     client.create_tweet(text=tweet, user_auth=True)
 
-
-if __name__ == '__main__':
+def work():
     find_following()
     pick_users()
     create_tweet()
@@ -43,3 +49,11 @@ if __name__ == '__main__':
     print("\tUsers: " + str(len(users)))
     print("\tCharacter count: " + str(len(tweet)))
     print(tweet)
+
+
+if __name__ == '__main__':
+    schedule.every().day.at("23:15:00").do(work)
+    while True:
+        schedule.run_pending()
+        time.sleep(10)
+
